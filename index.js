bce44ec51237198d380ad01cb3167f075c083f6f
naiemjoy1 <naiemunhasan@gmail.com>
2024-09-18 16:58:50 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 3000;
+
+// middleware
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://care-quest-2ae20.web.app",
+      "https://care-quest-2ae20.firebaseapp.com",
+    ],
+    credentials: true,
+  })
+);
+app.use(express.json());
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zcznn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    // collection
+    const productCollection = client.db("giftap_DB").collection("products");
+
+    // products api
+    app.get("/products", async (req, res) => {
+      const result = await productCollection.find().toArray();
+      res.send(result);
+    });
+
+    // app.get("/users", verifyToken, verifyAdmin, async (req, res) => {
+    //     const result = await userCollection.find().toArray();
+    //     res.send(result);
+    //   });
+
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("giftap Server Running");
+});
+
+app.listen(port, () => {
+  console.log(`giftap sitting on server port ${port}`);
+});
