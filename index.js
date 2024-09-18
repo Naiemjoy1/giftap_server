b23206908efb7a88cffb81f2d46edff29aad237a
naiemjoy1 <naiemunhasan@gmail.com>
2024-09-18 18:50:20 +0600

    brach create

--- index.js
+++ index.js
@@ -49,6 +49,8 @@ async function run() {
     //     res.send(result);
     //   });
 
+    // emni
+
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
