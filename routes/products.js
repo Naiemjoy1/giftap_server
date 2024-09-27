b6e2b32914f6da07ad0893420abf5048629dd3d1
naiemjoy1 <naiemunhasan@gmail.com>
2024-09-27 18:30:22 +0600

    search bar search suggetion added

--- routes/products.js
+++ routes/products.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const { ObjectId } = require("mongodb");
 const { client } = require("../config/db");
 
 const productCollection = client.db("giftap_DB").collection("products");
@@ -9,6 +10,11 @@ router.get("/", async (req, res) => {
   res.send(result);
 });
 
-module.exports = router;
+router.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) };
+  const result = await productCollection.findOne(query);
+  res.send(result);
+});
 
-// addddddddddd branch
+module.exports = router;
