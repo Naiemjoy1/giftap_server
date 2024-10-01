f89575b8e061efebffbfc68a62dcd9d310a12121
RashedulHaqueRasel1 <rashedulhaquerasel1@gmail.com>
2024-10-01 13:03:53 +0600

    Blog & Blog a datetiles page added

---
+++ routes/blogs.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const { ObjectId } = require("mongodb");
+const { client } = require("../config/db");
+
+const blogCollection = client.db("giftap_DB").collection("blogs");
+
+router.get("/", async (req, res) => {
+  const result = await blogCollection.find().toArray();
+  res.send(result);
+});
+
+
+router.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) };
+  const result = await blogCollection.findOne(query);
+  res.send(result);
+});
+
+module.exports = router;
