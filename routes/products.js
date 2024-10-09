2fe11600e9a361342f9757f4d66f1b54a8c7341b
naiemjoy1 <naiemunhasan@gmail.com>
2024-10-09 13:47:39 +0600

    seller request and handle this and store seller data

--- routes/products.js
+++ routes/products.js
@@ -17,4 +17,11 @@ router.get("/:id", async (req, res) => {
   res.send(result);
 });
 
+router.delete("/:id", async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) };
+  const result = await productCollection.deleteOne(query);
+  res.send(result);
+});
+
 module.exports = router;
