c9881eb439ed663949be1fc91e223f79ee5f4eb4
RashedulHaqueRasel1 <rashedulhaquerasel1@gmail.com>
2024-10-20 01:16:13 +0600

    get(show) complain data in api

--- routes/complain.js
+++ routes/complain.js
@@ -5,6 +5,10 @@ const { client } = require("../config/db");
 
 const complainCollection = client.db("giftap_DB").collection("complain");
 
+router.get("/", async (req, res) => {
+    const result = await complainCollection.find().toArray();
+    res.send(result);
+});
 
 // Save MongoDB complainCollection
 router.post("/", async (req, res) => {
