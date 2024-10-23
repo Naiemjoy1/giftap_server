0256880b41fba96ae377769b0f1921e611c558d5
arifadnan05 <arifahamedadnan@gmail.com>
2024-10-23 12:07:52 +0600

    baner advertise get api created

--- routes/banner.js
+++ routes/banner.js
@@ -6,10 +6,15 @@ const { client } = require("../config/db");
 const bannerCollection = client.db("giftap_DB").collection("banner");
 
 router.post("/", async (req, res) => {
-    const reviewData = req.body;
-    const result = await bannerCollection.insertOne(reviewData);
+    const bannerData = req.body;
+    const result = await bannerCollection.insertOne(bannerData);
     res.send(result);
 });
+
+router.get("/", async (req, res) => {
+    const result = await bannerCollection.find().toArray();
+    res.send(result)
+})
 
 
 module.exports = router;
