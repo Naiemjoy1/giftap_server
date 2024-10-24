46a110b4a1fc065c47c42ac6bddfd739f2568d6c
Mohammad Salim <mohammadsalim017427@gmail.com>
2024-10-24 10:36:53 +0600

    post and get api create for offers

--- routes/offers.js
+++ routes/offers.js
@@ -4,4 +4,15 @@ const router = express.Router();
 const { client } = require("../config/db");
 const { ObjectId } = require("mongodb");
 
-const productCollection = client.db("giftap_DB").collection("offers");
+const offerCollection = client.db("giftap_DB").collection("offers");
+
+router.post("/offers",async(req,res)=>{
+    const newOffer = req.body;
+    const result = await offerCollection.insertOne(newOffer);
+    res.send(result);
+})
+
+router.get("/offers",async(req,res)=>{
+   const result = await offerCollection.find().toArray();
+   res.send(result);
+})
